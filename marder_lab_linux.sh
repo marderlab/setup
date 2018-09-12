e95ac24d06a007b15134358fa7c1d493d57b1deb
gitlab@srinivas.gs <code@srinivas.gs>
2018-09-12 17:14:10 -0400

    bugfixes

--- marder_lab_linux.sh
+++ marder_lab_linux.sh
@@ -8,80 +8,83 @@
 # sudo apt install git
 
 # make code directory and get code
-if [ ! -d "~/code/" ]; then
-
-  mkdir ~/code/
+if [ ! -d "$HOME/code/" ]
+then
+	echo "making code directory..."
+  	mkdir ~/code/
 fi
-cd ~/code
 
-if [ ! -d "~/code/srinivas.gs_mtools" ]; then
-	# Control will enter here if $DIRECTORY exists.
+if [ ! -d "$HOME/code/srinivas.gs_mtools" ]
+then
+	echo "Installing mtools..."
+	cd $HOME/code/
 	git clone https://github.com/sg-s/srinivas.gs_mtools
 else
-	cd ~/code/srinivas.gs_mtools
+	echo "Updating mtools..."
+	cd $HOME/code/srinivas.gs_mtools
 	git stash
 	git checkout master
 	git pull
 fi
 
-if [ ! -d "~/code/xolotl" ]; then
+
+if [ ! -d "$HOME/code/xolotl" ]; then
 	# Control will enter here if $DIRECTORY exists.
+	echo "Installing xolotl..."
+	cd $HOME/code/
 	git clone https://github.com/sg-s/xolotl
 else 
-	cd ~/code/xolotl
+	cd $HOME/code/xolotl
 	git stash
 	git checkout master
 	git pull
 fi
 
 
-if [ ! -d "~/code/cpplab" ]; then
-	# Control will enter here if $DIRECTORY exists.
+if [ ! -d "$HOME/code/cpplab" ]; then
+	echo "Installing cpplab..."
+	cd $HOME/code/
 	git clone https://github.com/sg-s/cpplab
 else 
-	cd ~/code/cpplab
+	cd $HOME/code/cpplab
 	git stash
 	git checkout master
 	git pull
 fi
 
 
-if [ ! -d "~/code/crabsort" ]; then
-
+if [ ! -d "$HOME/code/crabsort" ]; then
+	echo "Installing crabsort..."
+	cd $HOME/code/
 	git clone https://github.com/sg-s/crabsort
 else 
-	cd ~/code/crabsort
+	cd $HOME/code/crabsort
 	git stash
 	git checkout master
 	git pull
 fi
 
-if [ ! -d "~/code/psychopomp" ]; then
 
+if [ ! -d "$HOME/code/psychopomp" ]; then
+	echo "Installing psychopomp..."
+	cd $HOME/code/
 	git clone https://github.com/sg-s/psychopomp
 else 
-	cd ~/code/psychopomp
+	cd $HOME/code/psychopomp
 	git stash
 	git checkout master
 	git pull
 fi
 
-if [ ! -d "~/code/dotfiles" ]; then
 
-	git clone https://github.com/sg-s/dotfiles
-else 
-	cd ~/code/dotfiles
-	git stash
-	git checkout master
-	git pull
-fi
 
 # install oh-my-git
-if [ ! -d "~/code/oh-my-git" ]; then
-
+if [ ! -d "$HOME/code/oh-my-git" ]; then
+	echo "Installing oh-my-git..."
+	cd $HOME/code/
 	git clone https://github.com/arialdomartini/oh-my-git
 else 
-	cd ~/code/oh-my-git
+	cd $HOME/code/oh-my-git
 	git stash
 	git checkout master
 	git pull
@@ -89,7 +92,7 @@ fi
 
 
 # configure the shell to behave nicely 
-rm ~/.bash_profile
-rm ~/.bashrc
+rm $HOME/.bash_profile
+rm $HOME/.bashrc
 
-mv marder_lab_bashrc.sh ~/.bashrc
+mv $HOME/setup/marder_lab_bashrc.sh $HOME/.bashrc
